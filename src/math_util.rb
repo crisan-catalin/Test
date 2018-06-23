8214a1161190f5366418452c3f1b0028cad7c253
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-06-23 12:11:50 +0300

    Implement method to get two largest numbers from array

--- src/math_util.rb
+++ src/math_util.rb
@@ -4,4 +4,26 @@ class MathUtil
     numbers&.select(&:even?)&.sum
   end
 
+  def self.two_largest_numbers(numbers)
+    return nil if numbers.nil? ||  numbers.size < 2
+
+    firstNumber = numbers[0]
+    secondNumber = numbers[1]
+
+    if firstNumber < secondNumber
+      firstNumber, secondNumber = secondNumber, firstNumber
+    end
+
+    numbers[2...numbers.size].each { |x|
+      if x > firstNumber
+        secondNumber = firstNumber
+        firstNumber = x
+      elsif x > secondNumber
+        secondNumber = x
+      end
+    }
+
+    [firstNumber, secondNumber]
+  end
+
 end
