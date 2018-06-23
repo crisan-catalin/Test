9368da8bd5647106e79235a3072ae21e8170d921
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-06-23 12:36:42 +0300

    Test methods to check if exists duplicates

--- test/math_util_test.rb
+++ test/math_util_test.rb
@@ -47,4 +47,30 @@ class TestMathUtil < Test::Unit::TestCase
     assert_equal([-1, -2], MathUtil.two_largest_numbers([-1, -2, -3, -4]))
   end
 
+# Test exists_duplicates
+
+  def test_exists_duplicates_returns_nil
+    assert_nil(MathUtil.exists_duplicates(nil))
+  end
+
+  def test_exists_duplicates_one_element_returns_nil
+    assert_equal(nil, MathUtil.exists_duplicates([0]))
+  end
+
+  def test_exists_duplicates_is_true
+    assert_true(MathUtil.exists_duplicates([1, 2, 3, 1]))
+  end
+
+  def test_exists_duplicates_with_multiple_duplicates_is_true
+    assert_true(MathUtil.exists_duplicates([1, 2, 3, 1, 1, 1, 2, 3, 1]))
+  end
+
+  def test_exists_duplicates_is_false
+    assert_false(MathUtil.exists_duplicates([1, 2, 3]))
+  end
+
+  def test_exists_duplicates_with_negative_duplicates_is_true
+    assert_true(MathUtil.exists_duplicates([-1, 2, 3, -1]))
+  end
+
 end
