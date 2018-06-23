0dabc75d19a9245c62ad4935df4570c471ca398d
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-06-23 12:36:29 +0300

    Test methods to get two largest numbers

--- test/math_util_test.rb
+++ test/math_util_test.rb
@@ -25,4 +25,26 @@ class TestMathUtil < Test::Unit::TestCase
     assert_equal(8, MathUtil.sum_even_numbers([0, -2, 4, 6]))
   end
 
+# Test two_largest_numbers
+
+  def test_two_largest_numbers_returns_nil
+    assert_nil(MathUtil.two_largest_numbers(nil))
+  end
+
+  def test_two_largest_numbers_empty_array_returns_nil
+    assert_equal(nil, MathUtil.two_largest_numbers([]))
+  end
+
+  def test_two_largest_numbers_one_element_returns_nil
+    assert_equal(nil, MathUtil.two_largest_numbers([0]))
+  end
+
+  def test_two_largest_numbers_returns_4_and_3
+    assert_equal([4, 3], MathUtil.two_largest_numbers([0, 1, 2, 3, 4]))
+  end
+
+  def test_two_largest_numbers_returns_minus_1_and_minus_2
+    assert_equal([-1, -2], MathUtil.two_largest_numbers([-1, -2, -3, -4]))
+  end
+
 end
