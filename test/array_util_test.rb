aadfade83e6f140f2329e2f0566df526c3f37d1a
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-06-24 17:27:42 +0300

    TestArrayUtil: Remove tests for nil case and implement tests for other cases
    Implement methods for:
    - get same element when array has one element for two_largest_numbers
    - check sorted and unsorted array for two_largest_numbers
    - test exists_duplicates when array has one element

--- test/array_util_test.rb
+++ test/array_util_test.rb
@@ -5,10 +5,6 @@ class TestArrayUtil < Test::Unit::TestCase
 
 # Test sum_even_numbers
 
-  def test_sum_even_numbers_nil_value_returns_nil
-    assert_nil(ArrayUtil.sum_even_numbers(nil))
-  end
-
   def test_sum_even_numbers_empty_array_returns_0
     assert_equal(0, ArrayUtil.sum_even_numbers([]))
   end
@@ -27,20 +23,16 @@ class TestArrayUtil < Test::Unit::TestCase
 
 # Test two_largest_numbers
 
-  def test_two_largest_numbers_nil_value_returns_nil
-    assert_nil(ArrayUtil.two_largest_numbers(nil))
-  end
-
-  def test_two_largest_numbers_empty_array_returns_nil
-    assert_equal(nil, ArrayUtil.two_largest_numbers([]))
-  end
-
-  def test_two_largest_numbers_one_element_array_returns_nil
+  def test_two_largest_numbers_one_element_array_returns_same_element
     assert_equal(nil, ArrayUtil.two_largest_numbers([0]))
   end
 
-  def test_two_largest_numbers_positive_numbers_array_returns_4_and_3
+  def test_two_largest_numbers_positive_numbers_sorted_array_returns_4_and_3
     assert_equal([4, 3], ArrayUtil.two_largest_numbers([0, 1, 2, 3, 4]))
+  end
+
+  def test_two_largest_numbers_positive_numbers_unsorted_array_returns_4_and_3
+    assert_equal([4, 3], ArrayUtil.two_largest_numbers([0, 4, 2, 3]))
   end
 
   def test_two_largest_numbers_negative_numbers_array_returns_minus_1_and_minus_2
@@ -49,12 +41,8 @@ class TestArrayUtil < Test::Unit::TestCase
 
 # Test exists_duplicates
 
-  def test_exists_duplicates_nil_value_returns_nil
-    assert_nil(ArrayUtil.exists_duplicates(nil))
-  end
-
-  def test_exists_duplicates_one_element_array_returns_nil
-    assert_equal(nil, ArrayUtil.exists_duplicates([0]))
+  def test_exists_duplicates_one_element_array_is_false
+    assert_true(ArrayUtil.exists_duplicates([1]))
   end
 
   def test_exists_duplicates_with_a_duplicate_is_true
@@ -67,10 +55,6 @@ class TestArrayUtil < Test::Unit::TestCase
 
   def test_exists_duplicates_without_duplicates_is_false
     assert_false(ArrayUtil.exists_duplicates([1, 2, 3]))
-  end
-
-  def test_exists_duplicates_with_negative_duplicates_is_true
-    assert_true(ArrayUtil.exists_duplicates([-1, 2, 3, -1]))
   end
 
 end
