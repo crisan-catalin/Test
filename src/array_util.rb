5d86797fdfa52d0e1c72a9cfd03d5af52f609b93
Crisan G. Catalin <catacrisan_catacrsn@yahoo.com>
2018-06-24 14:59:10 +0300

    ArrayUtil: Refactor each iterator format

--- src/array_util.rb
+++ src/array_util.rb
@@ -14,14 +14,14 @@ class ArrayUtil
       firstNumber, secondNumber = secondNumber, firstNumber
     end
 
-    numbers[2...numbers.size].each { |x|
+    numbers[2...numbers.size].each do |x|
       if x > firstNumber
         secondNumber = firstNumber
         firstNumber = x
       elsif x > secondNumber
         secondNumber = x
       end
-    }
+    end
 
     [firstNumber, secondNumber]
   end
@@ -29,13 +29,13 @@ class ArrayUtil
   def self.exists_duplicates(numbers)
     return nil if numbers.nil? || numbers.size < 2
 
-    numbers.each { |x|
+    numbers.each do |x|
       if numbers[x.abs - 1].to_i < 0
         return true
       else
         numbers[x.abs - 1] = numbers[x.abs - 1].to_i * (-1)
       end
-    }
+    end
 
     false
   end
